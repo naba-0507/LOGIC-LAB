0dd8d79163c6c44e5b116e6c9d92fc08d80323cf
naba-0507 <nabaa.gts@gmail.com>
2026-01-07 18:58:13 +0500

    Update scoreboard.js

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,5 +1,5 @@
 // Load username
-let username = localStorage.getItem("currentuser");
+let username = localStorage.getItem("currentUser");
 document.getElementById("displayName").innerText =
     username ? "Welcome, " + username : "Guest";
 
@@ -21,5 +21,6 @@ if (logicScore !== null) {
     let calculated = total > 0 ? Math.round((correct / total) * 100) : 0;
     document.getElementById("logicScore").innerText = calculated;
 }
+
 
 
